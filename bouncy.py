a96f2bbd406c18dfff3577fd81e5a09be360032e
Lillian Jiang <ljiang@ucvts.tec.nj.us>
2019-12-09 10:17:30 -0500

    bouncing picture

--- bouncy.py
+++ bouncy.py
@@ -15,7 +15,7 @@ white = (255, 255, 255)
 screen = pygame.display.set_mode( (width, height) )
 
 # Load an image
-ball = pygame.image.load("img/ball.png")
+ball = pygame.image.load("img/patagonia.jpg")
 ballrect = ball.get_rect()
 
 while True:
